7d94501eecc778b01b2dbe5c031d393a5ab6d150
gortron <gordon.lanza@gmail.com>
2019-11-18 14:40:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your code here!
+let element = document.createElement("div");
+
+document.body.appendChild(element);
+
+let ul = document.createElement("ul");
+
+for (let i = 0; i < 3; i++) {
+  let li = document.createElement("li");
+  li.innerHTML = (i + 1).toString();
+  ul.appendChild(li);
+}
+
+element.appendChild(ul);
+
+element.style.backgroundColor = "#27647B";
+element.style.textAlign = "center";
+ul.style.textAlign = "left";
+
+ul.removeChild(ul.querySelector("li:nth-child(2)"));
+
+document.querySelector("#main").remove();
+
+let newHeader = document.querySelector("#victory");
+newHeader.innerHTML = "GORDY is the champion";
