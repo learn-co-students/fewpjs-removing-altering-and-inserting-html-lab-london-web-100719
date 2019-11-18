eca61a7cf14d261afff9459e68f7837f6f5b7413
Peter Sampson <peter.sampson010@gmail.com>
2019-11-18 15:10:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+let main = document.querySelector('#main')
+main.remove()
+
+let newHeader = document.createElement('h1')
+newHeader.className = "victory"
+
+newHeader.id = "victory"
+
+newHeader.innerHTML += " You is the champion"
